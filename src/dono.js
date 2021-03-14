0181f8c5fff524cb93fd14405ae8d7270752c29f
RafaModz <daguiladanightbot@gmail.com>
2021-03-14 12:21:07 -0300

    First commit

---
+++ src/dono.js
@@ -0,0 +1,9 @@
+const dono  = (prefix, sender) => {
+	return `DONO
+
+CRIADOR DO BOT: BY CHAROPE
+BASE: BY CHAROPE
+MEU INSTA: CHAROPE-_-MODZ`
+}
+
+exports.dono = dono
---
+++ src/help2.js
@@ -0,0 +1,73 @@
+const help2 = (prefix, sender) => {
+	return `OUTROS COMANDOS🔥🇧🇷
+
+BORA LA PROS OUTROS COMANDOS😁
+=============================
+COMANDO DE VOZ SEM O (!)
+
+> bom dia 
+> boa tarde
+> boa noite
+> membro novo 
+> canta 
+> mc poze 
+_____________________
+COMANDO COM O (!)
+
+     [IMAGENS]
+🔧 ESTA POR VIM 
+==COMANDOS EM TESTE=========
+=========================
+
+============================
+[!]STICKER
+[!]TOIMG
+[!]NSFWMENU
+[!]MENUADMIN
+[!]DONO
+
+
+======
+CRIAR]
+=======
+[!]toimg
+[!]bpink
+[!]marvellogo
+[!]snowwrite
+[!]3dtext
+[!]ninjalogo
+[!]water
+[!]firetext
+[!]neonlogo
+[!]lionlogo
+[!]jokerlogo
+[!]quotemaker
+
+
+======
+BAIXAR]
+=======
+[!]images
+[!]ytmp3
+[!]ytmp4
+[!]joox
+[!]joox2
+[!]trendtwit
+[!]ytsearch
+_____________________________
+
+C]----M☯︎
+H]----A☯︎
+A]----N☯︎
+R]----D☯︎
+O]----A☯︎
+P]----☯︎
+E]----☯︎
+
+ESPERO QUE GOSTE DO BOT :) 
+MEU INSTA CHAROPE-_-MODZ
+[QUALQUER DUVIDAS ME CHAMA NO PV 
+`
+}
+
+exports.help2 = help2
